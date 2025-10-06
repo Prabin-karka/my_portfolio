110d80e72d6741779e6c86867c152f3ba997fa16
prabinraj <r.rajaprabinraj@gmail.com>
2025-10-06 19:21:36 +0530

    ee

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 
-
 function App() {
+
+  window.onload = function() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+        }
+      });
+    }, { threshold: 0.3 });
+
+    document.querySelectorAll(".fade-in").forEach(el => observer.observe(el));
+  };
+
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
+
+
+// import React from "react";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Home from "./components/Home";
+// import Login from "./components/Login";
+// import Signup from "./components/Signup";
+
+
+// function App() {
 
   // document.addEventListener("DOMContentLoaded", () => {
   //   const observer = new IntersectionObserver((entries) => {
@@ -22,28 +57,28 @@ function App() {
   // });
     
 
-  window.onload = function() {
-    const observer = new IntersectionObserver((entries) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add("show");
-        }
-      });
-    }, { threshold: 0.3 });
+//   window.onload = function() {
+//     const observer = new IntersectionObserver((entries) => {
+//       entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//           entry.target.classList.add("show");
+//         }
+//       });
+//     }, { threshold: 0.3 });
   
-    document.querySelectorAll(".fade-in").forEach(el => observer.observe(el));
-  };
+//     document.querySelectorAll(".fade-in").forEach(el => observer.observe(el));
+//   };
   
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login/>} />
-        <Route path="signup" element={<Signup/>}/>
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="login" element={<Login/>} />
+//         <Route path="signup" element={<Signup/>}/>
 
-      </Routes>
-    </Router>
-  );
-}
+//       </Routes>
+//     </Router>
+//   );
+// }
 
-export default App;
+// export default App;
