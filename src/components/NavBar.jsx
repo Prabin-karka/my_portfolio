576635a5c6b0fab787b82cecb39b3259ca2bc11d
Prabin-karka <r.rajaprabinraj@gmail.com>
2025-10-16 11:39:03 +0530

    navbar fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-scroll"; 
+import { Link } from "react-scroll";
 import { useNavigate } from "react-router-dom";
 import "../styles/NavBar.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
-// import { animateScroll as scroll } from "react-scroll";
-
 
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -14,7 +12,12 @@ const NavBar = () => {
 
   const toggleMenu = () => {
     console.log("Hamburger clicked");
-    setIsOpen(!isOpen);};
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
 
   useEffect(() => {
     const handleScroll = () => {
@@ -27,9 +30,12 @@ const NavBar = () => {
 
   return (
     <nav className={`navbar ${scrolled ? "scrolled" : ""}`}>
-      {/* <button className="logo" onClick={() => scroll.scrollToTop()}>Super Folio</button> */}
-      <button className="logo" onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>Portfolio</button>
-
+      <button
+        className="logo"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
+        Portfolio
+      </button>
 
       <div className="hamburger" onClick={toggleMenu}>
         <div></div>
@@ -38,24 +44,29 @@ const NavBar = () => {
       </div>
 
       <div className={`nav-links ${isOpen ? "show" : ""}`}>
-        <Link to="about" smooth={true} duration={500} offset={-30}>About</Link>
-        {/* <Link to="services" smooth={true} duration={500} offset={-30}>Services</Link> */}
-        <Link to="myskill" smooth={true} duration={500} offset={-65}>Skills</Link>
-        <Link to="portfolio" smooth={true} duration={500} offset={-85}>Project</Link>
-        <Link to="experience" smooth={true} duration={500} offset={-65}>Experience</Link>
-        <Link to="contact" smooth={true} duration={500} offset={-50}>Contact</Link>
-        {/* <button onClick={() => navigate("/login")} className="navlogin-button">
-        <FontAwesomeIcon icon={faCircleUser} /> <h3 className="hamlogin">Login</h3>
-      </button> */}
+        <Link to="about" smooth={true} duration={500} offset={-30} onClick={closeMenu}>
+          About
+        </Link>
+        <Link to="myskill" smooth={true} duration={500} offset={-65} onClick={closeMenu}>
+          Skills
+        </Link>
+        <Link to="portfolio" smooth={true} duration={500} offset={-85} onClick={closeMenu}>
+          Project
+        </Link>
+        <Link to="experience" smooth={true} duration={500} offset={-65} onClick={closeMenu}>
+          Experience
+        </Link>
+        <Link to="contact" smooth={true} duration={500} offset={-50} onClick={closeMenu}>
+          Contact
+        </Link>
+
+        {/* If you re-enable login button */}
+        {/* <button onClick={() => { navigate("/login"); closeMenu(); }} className="navlogin-button">
+          <FontAwesomeIcon icon={faCircleUser} /> <h3 className="hamlogin">Login</h3>
+        </button> */}
       </div>
     </nav>
   );
 };
 
 export default NavBar;
-
-
-
-
-
-
